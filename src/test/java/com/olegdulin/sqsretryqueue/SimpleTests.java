269f464b9f92ce4429aee64ca536bfef842e97d5
Oleg Dulin <oleg.dulin@gmail.com>
2015-05-09 14:22:32 -0400

    Added a test for failures

--- src/test/java/com/olegdulin/sqsretryqueue/SimpleTests.java
+++ src/test/java/com/olegdulin/sqsretryqueue/SimpleTests.java
@@ -14,6 +14,45 @@ import com.amazonaws.regions.Regions;
 public class SimpleTests {
 
 	@Test
+	public void allFailuresRetried() throws InterruptedException {
+		SQSRetryQueue srq = new SQSRetryQueue();
+		srq.setSqsQueueName("TEST-allFailuresRetried");
+		srq.setVisibilityTimeout(2);
+		srq.setMessageReceiverThreads(4);
+		srq.setRegion(Regions.US_EAST_1);
+		srq.init();
+		srq.startListening();
+		ConcurrentHashMap<String, Boolean> messageStatus = new ConcurrentHashMap<>();
+		ConcurrentHashMap<String, Boolean> retries = new ConcurrentHashMap<>();
+		srq.setMessageReceiverCallable(new MessageReceiverCallable() {
+
+			@Override
+			public Boolean call() throws Exception {
+				String messageBody = getMessageBody();
+				if (!retries.containsKey(messageBody)) {
+					retries.put(messageBody, true);
+					throw new Exception();
+				}
+				messageStatus.put(messageBody, true);
+				return true;
+			}
+		});
+		for (int i = 0; i < 10; i++) {
+			String messageBody = "message #" + i;
+			messageStatus.put(messageBody, false);
+			srq.sendMessage(messageBody);
+		}
+		Logger.getLogger(getClass().getName()).info(
+				"Waiting for things to settle down");
+		Thread.sleep(30 * 1000);
+		for (Entry<String, Boolean> entry : messageStatus.entrySet()) {
+			if (!entry.getValue()) {
+				fail("Not all messages have been received");
+			}
+		}
+	}
+
+	@Test
 	public void allMessagesReceived() throws InterruptedException {
 		SQSRetryQueue srq = new SQSRetryQueue();
 		srq.setSqsQueueName("TEST-allMessagesReceived");
@@ -22,7 +61,7 @@ public class SimpleTests {
 		srq.setRegion(Regions.US_EAST_1);
 		srq.init();
 		srq.startListening();
-		ConcurrentHashMap<String, Boolean> messageStatus=new ConcurrentHashMap<>();
+		ConcurrentHashMap<String, Boolean> messageStatus = new ConcurrentHashMap<>();
 		srq.setMessageReceiverCallable(new MessageReceiverCallable() {
 
 			@Override
@@ -34,18 +73,17 @@ public class SimpleTests {
 		});
 		for (int i = 0; i < 10; i++) {
 			String messageBody = "message #" + i;
-			messageStatus.put(messageBody, false);			
+			messageStatus.put(messageBody, false);
 			srq.sendMessage(messageBody);
 		}
-		Logger.getLogger(getClass().getName()).info("Waiting for things to settle down");
-		Thread.sleep(srq.getVisibilityTimeout()*1000);
-		for (Entry<String, Boolean> entry : messageStatus.entrySet())
-		{
-			if (!entry.getValue())
-			{
+		Logger.getLogger(getClass().getName()).info(
+				"Waiting for things to settle down");
+		Thread.sleep(srq.getVisibilityTimeout() * 1000);
+		for (Entry<String, Boolean> entry : messageStatus.entrySet()) {
+			if (!entry.getValue()) {
 				fail("Not all messages have been received");
 			}
-		}		
+		}
 	}
 
 }
